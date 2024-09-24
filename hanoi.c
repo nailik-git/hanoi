f99541661acf586507f60ba907a439afff518089
segfault-enjoyer <103898002+segfault-enjoyer@users.noreply.github.com>
2024-09-24 16:13:18 +0200

    Removed all allocations

--- hanoi.c
+++ hanoi.c
@@ -24,7 +24,7 @@ int pop(stack *this) {
 typedef struct hanoi {
   int height;
   unsigned long int count;
-  stack* towers[3];
+  stack towers[3];
   int (*checkMove)(struct hanoi *this, int from, int to);
   void (*moveWithCheck)(struct hanoi *this, int from, int to);
   void (*move)(struct hanoi *this, int from, int to);
@@ -35,8 +35,8 @@ typedef struct hanoi {
 int checkMove(hanoi* this, int from, int to) {
   if(from < 0 || from > 2) return 0;
   if(to < 0 || to > 2) return 0;
-  const int a = this->towers[from]->array[this->towers[from]->top];
-  const int b = this->towers[to]->array[this->towers[to]->top];
+  const int a = this->towers[from].array[this->towers[from].top];
+  const int b = this->towers[to].array[this->towers[to].top];
   if(a > b) return 0;
   return 1;
 }
@@ -44,55 +44,54 @@ int checkMove(hanoi* this, int from, int to) {
 void moveWithCheck(hanoi* this, int from, int to) {
   if(!checkMove(this, from, to)) return;
   this->count++;
-  this->towers[to]->push(this->towers[to], this->towers[from]->pop(this->towers[from]));
+  this->towers[to].push(&this->towers[to], this->towers[from].pop(&this->towers[from]));
 }
 
 void move(hanoi* this, int from, int to) {
   this->count++;
-  this->towers[to]->push(this->towers[to], this->towers[from]->pop(this->towers[from]));
+  this->towers[to].push(&this->towers[to], this->towers[from].pop(&this->towers[from]));
 }
 
 int status(hanoi* this) {
-  return this->towers[1]->top + 1 == this->height || this->towers[2]->top + 1 == this->height;
+  return this->towers[1].top + 1 == this->height || this->towers[2].top + 1 == this->height;
 }
 
 void print(hanoi* h) {
   for (int j = h->height - 1; j >= 0; j--) {
     for(int i = 0; i < 3; i++) {
-      if(h->towers[i]->top < j) {
+      if(h->towers[i].top < j) {
         for(int n = 0; n < h->height; n++) printf(" ");
         printf("0");
         for(int n = 0; n < h->height; n++) printf(" ");
         continue;
       }
-      int disk = h->towers[i]->array[j];
+      int disk = h->towers[i].array[j];
       const int shift = disk > 9;
       for(int n = 0; n < h->height - disk; n++) printf(" ");
-      for(int n = 0; n < h->towers[i]->array[j] - shift; n++) printf("-");
-      printf("%d", h->towers[i]->array[j]);
-      for(int n = 0; n < h->towers[i]->array[j]; n++) printf("-");
+      for(int n = 0; n < h->towers[i].array[j] - shift; n++) printf("-");
+      printf("%d", h->towers[i].array[j]);
+      for(int n = 0; n < h->towers[i].array[j]; n++) printf("-");
       for(int n = 0; n < h->height - disk; n++) printf(" ");
     }
     printf("\n");
   }
 }
 
-hanoi* initialize(int height) {
-  hanoi* r = malloc(sizeof(struct hanoi));
-  r->height = height;
-  r->count = 0;
-  r->checkMove = checkMove;
-  r->moveWithCheck = moveWithCheck;
-  r->move = move;
-  r->status = status;
-  r->print = print;
+hanoi initialize(int height) {
+  hanoi r;
+  r.height = height;
+  r.count = 0;
+  r.checkMove = checkMove;
+  r.moveWithCheck = moveWithCheck;
+  r.move = move;
+  r.status = status;
+  r.print = print;
   for(int i = 0; i < 3; i++) {
-    r->towers[i] = malloc(sizeof(struct stack));
-    r->towers[i]->top = -1; 
-    r->towers[i]->push = push; 
-    r->towers[i]->pop = pop;
+    r.towers[i].top = -1; 
+    r.towers[i].push = push; 
+    r.towers[i].pop = pop;
   }
-  for(int i = height; i > 0; i--) push(r->towers[0], i);
+  for(int i = height; i > 0; i--) push(&r.towers[0], i);
   return r;
 }
 
@@ -118,31 +117,23 @@ void solveRecursive(hanoi* h, int disk, int from, int to, int aux) {
   solveRecursive(h, disk - 1, aux, to, from);
 }
 
-void game(hanoi* h) {
-  
-}
-
-void end(hanoi *h) {
-  for(int i = 0; i < 3; i++) {
-    free(h->towers[i]);
-  }
-  free(h);
-}
+void game(hanoi* h) {}
 
 int main(int argc, char** argv) {
-  if(argc != 3) {printf("usage: hanoi {user, iterative, recursive} {number of plates}"); return -1;}
+  if(argc != 3) {
+    printf("usage: hanoi {user, iterative, recursive} {number of plates}\n");
+    return -1;
+  }
   char* output;
   int height = (int) strtol(argv[2], &output, 10);
-  hanoi* h = initialize(height);
-  h->print(h);
+  hanoi h = initialize(height);
+  h.print(&h);
   printf("\x1b[%dF", height);
 
-  if(strcmp(argv[1], "user") == 0) game(h);
-  if(strcmp(argv[1], "iterative") == 0) solveIterative(h);
-  if(strcmp(argv[1], "recursive") == 0) solveRecursive(h, height, 0, 2, 1);
-  h->print(h);
-  
-  end(h);
+  if(strcmp(argv[1], "user") == 0) game(&h);
+  if(strcmp(argv[1], "iterative") == 0) solveIterative(&h);
+  if(strcmp(argv[1], "recursive") == 0) solveRecursive(&h, height, 0, 2, 1);
+  h.print(&h);
   
   return 0;
 }

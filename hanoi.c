2586132ce639ba4dfb45e4fecd72cbc57166ecf0
NailikGit <kilian.smeets@web.de>
2025-02-19 01:16:12 +0100

    refactor

--- hanoi.c
+++ hanoi.c
@@ -9,13 +9,13 @@ typedef struct stack {
   int array[64];
 } stack;
 
-void push(stack *s, int disk) {
+void push(stack* s, int disk) {
   // pushes disk onto the stack
   s->top++;
   s->array[s->top] = disk;
 }
 
-int pop(stack *s) {
+int pop(stack* s) {
   // pops disk from the stack
   const int r = s->array[s->top];
   s->top--;
@@ -24,12 +24,12 @@ int pop(stack *s) {
 
 // a struct to model the towers of hanoi game
 typedef struct hanoi {
-  const unsigned char height;
-  unsigned long int count; // number of steps
+  const char height;
+  uint64_t count; // number of steps
   stack towers[3];
 } hanoi;
 
-int checkMove(hanoi* h, int from, int to) {
+int check_move(hanoi* h, int from, int to) {
   // checks if a move is legal
   if(from < 0 || from > 2) return 0;
   if(to < 0 || to > 2) return 0;
@@ -41,9 +41,9 @@ int checkMove(hanoi* h, int from, int to) {
   return 1;
 }
 
-void moveWithCheck(hanoi* h, int from, int to) {
+void move_with_check(hanoi* h, int from, int to) {
   // moves disk from tower from to tower to and checks the move
-  if(!checkMove(h, from, to)) return;
+  if(!check_move(h, from, to)) return;
   h->count++;
   push(&h->towers[to], pop(&h->towers[from]));
 }
@@ -81,7 +81,7 @@ void print(hanoi* h) {
   }
 }
 
-hanoi initialize(unsigned char height) {
+hanoi initialize(char height) {
   // creates an object of the hanoi struct
   hanoi r = {
   .height = height,
@@ -94,18 +94,16 @@ hanoi initialize(unsigned char height) {
   return r;
 }
 
-int modLookup(int n) {
-  // look up table to mitigate mod operation for move function
-  const int lookup[5] = {0, 1, 2, 0, 1};
-  return lookup[n];
-}
-
-void solveIterative(hanoi* h) {
+void solve_iterative(hanoi* h) {
   // solves the towers of hanoi iteratively
   // constants:
-  const unsigned char height = h->height;
+  const char height = h->height;
   const uint64_t max = 0xFFFFFFFFFFFFFFFF;
   const uint64_t bound = max >> (64 - height); // max number of moves needed to solve
+
+  // look up table to mitigate mod operation for move function
+  const int lookup[5] = {0, 1, 2, 0, 1};
+
   uint64_t i = 0; // iterator
   for(; i < bound; i++) {
     const int disk = __builtin_ctz(i + 1) + 1; // gray codes method to find the disk to move
@@ -114,22 +112,22 @@ void solveIterative(hanoi* h) {
                                                           * h works somewhat similar to the gray codes,
                                                           * through how many times we have moved bigger disks 
                                                           * impossible according to wikipedia :) */
-    move(h, from, modLookup(from + 1 + move_direction));  
+    move(h, from, lookup[from + 1 + move_direction]);  
     //h->print(h);
     //printf("\x1b[%dF", h->height);
   }
   h->count = i;
 }
 
-void solveRecursive(hanoi* h, int disk, int from, int to, int aux) {
+void solve_recursive(hanoi* h, int disk, int from, int to, int aux) {
   // solves the towers of hanoi recursively
   if(disk == 0) return; // break cond
-  solveRecursive(h, disk - 1, from, aux, to); // move bigger disks
+  solve_recursive(h, disk - 1, from, aux, to); // move bigger disks
   move(h, from, to); // move h disk
   h->count++;
   //h->print(h);
   //printf("\x1b[%dF", h->height);
-  solveRecursive(h, disk - 1, aux, to, from); // move bigger disks again
+  solve_recursive(h, disk - 1, aux, to, from); // move bigger disks again
   }
 
 void game(hanoi* h) {
@@ -144,26 +142,26 @@ void game(hanoi* h) {
     scanf("%d", &from); // user input to move the disks
     printf("to tower: ");
     scanf("%d", &to);
-    moveWithCheck(h, --from, --to); // execute move with check
+    move_with_check(h, --from, --to); // execute move with check
     printf("\x1b[%dF", h->height + 2); // return cursor to top of the screen
     }
 }
 
 int main(int argc, char** argv) {
   if(argc != 3) {
-    printf("usage: hanoi {user, iterative, recursive} {number of plates (up to 64)}\n");
+    printf("usage: hanoi <user|iterative|recursive> <number of plates (less than 64)>\n");
     return -1;
   }
   char* output;
-  unsigned char height = (unsigned char) strtol(argv[2], &output, 10);
+  char height = (char) strtol(argv[2], &output, 10);
   if(height > 64) return -1;
   hanoi h = initialize(height);
   print(&h);
   printf("\x1b[%dF", height);
 
   if(strcmp(argv[1], "user") == 0) game(&h);
-  else if(strcmp(argv[1], "iterative") == 0) solveIterative(&h);
-  else if(strcmp(argv[1], "recursive") == 0) solveRecursive(&h, height, 0, 2, 1);
+  else if(strcmp(argv[1], "iterative") == 0) solve_iterative(&h);
+  else if(strcmp(argv[1], "recursive") == 0) solve_recursive(&h, height, 0, 2, 1);
   else {
     printf("usage: hanoi {user, iterative, recursive} {number of plates}\n");
     return -1;
